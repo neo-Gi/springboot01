c0724b1f7af76f221621c00a663cf8b6c0f7beb3
neo-Gi <1904193389@qq.com>
2020-05-23 16:01:01 +0800

    更新

--- src/main/java/com/gi/springboot01/controller/UserController.java
+++ src/main/java/com/gi/springboot01/controller/UserController.java
@@ -1,4 +1,8 @@
 package com.gi.springboot01.controller;
 
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
 }
---
+++ src/main/java/com/gi/springboot01/service/UserService.java
@@ -0,0 +1,7 @@
+package com.gi.springboot01.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+}
