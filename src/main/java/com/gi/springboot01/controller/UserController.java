cc9f4c0b53d7263f4f50a9ab6711e4636c5603f7
neo-Gi <1904193389@qq.com>
2020-05-23 16:31:32 +0800

    修改yml文件，建立getUser方法

--- src/main/java/com/gi/springboot01/controller/UserController.java
+++ src/main/java/com/gi/springboot01/controller/UserController.java
@@ -1,8 +1,16 @@
 package com.gi.springboot01.controller;
 
 
+import com.gi.springboot01.domain.User;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/UserController")
 public class UserController {
+    @RequestMapping("/getUser")
+    public User getUser(){
+        User user = new User("123","1234567890","13605153165");
+        return  user;
+    }
 }
