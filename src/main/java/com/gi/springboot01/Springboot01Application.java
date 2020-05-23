7021df96453a54402e83c62f6baa39a4cd412231
neo-Gi <1904193389@qq.com>
2020-05-23 15:32:40 +0800

    更新

--- src/main/java/com/gi/springboot01/Springboot01Application.java
+++ src/main/java/com/gi/springboot01/Springboot01Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Springboot01Application {
 
     public static void main(String[] args) {
+        System.out.println(1);
         SpringApplication.run(Springboot01Application.class, args);
     }
 
---
+++ src/main/java/com/gi/springboot01/controller/UserController.java
@@ -0,0 +1,4 @@
+package com.gi.springboot01.controller;
+
+public class UserController {
+}
