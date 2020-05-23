52c0e2a1d518cc00ac47b2579781a3f8beecc864
neo-Gi <1904193389@qq.com>
2020-05-23 16:42:57 +0800

    掉用Service接口，修改controller

--- src/main/java/com/gi/springboot01/controller/UserController.java
+++ src/main/java/com/gi/springboot01/controller/UserController.java
@@ -2,15 +2,21 @@ package com.gi.springboot01.controller;
 
 
 import com.gi.springboot01.domain.User;
+import com.gi.springboot01.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/UserController")
 public class UserController {
+
+    @Autowired
+    private UserService userService;
+
     @RequestMapping("/getUser")
     public User getUser(){
-        User user = new User("123","1234567890","13605153165");
+        User user = userService.getUser();
         return  user;
     }
 }
--- src/main/java/com/gi/springboot01/service/UserService.java
+++ src/main/java/com/gi/springboot01/service/UserService.java
@@ -1,7 +1,12 @@
 package com.gi.springboot01.service;
 
+import com.gi.springboot01.domain.User;
 import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
+    public User getUser() {
+        User user = new User("123","1234567890","13605153165");
+        return user;
+    }
 }
