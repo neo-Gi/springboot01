dfb17af8723afb8140df9b43d3be7b562e3d980e
neo-Gi <1904193389@qq.com>
2020-06-01 00:54:53 +0800

    新增了增查改功能,尚未测试

--- src/main/java/com/gi/springboot01/dao/StaffDao.java
+++ src/main/java/com/gi/springboot01/dao/StaffDao.java
@@ -3,8 +3,22 @@ package com.gi.springboot01.dao;
 import com.gi.springboot01.domain.Staff;
 import org.apache.ibatis.annotations.Mapper;
 
+import java.util.List;
+
 @Mapper
 public interface StaffDao {
+    /*
+    获取员工ID
+    */
     Staff getStaffByStaffId(String staffId);
+    /*
+    删除员工
+    */
     Staff deleteStaffByStaffId(String staffId);
+    /*
+    以下功能未测试
+    */
+    List<Staff> staffList();
+    int insertStaff();
+    int updateStaff();
 }
